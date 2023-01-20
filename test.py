af972cd02f28152338eb5b9afb92455b5e0de5bb
psmatter <github@ndxa.de>
2023-01-20 18:28:44 +0000

    Initial commit

---
+++ test.py
@@ -0,0 +1,24 @@
+from lib import splitter 
+text="This is a text.\n One line two sentences. We have more than one line. Here is another.\n We have more. \n"
+
+print("Test input to splitter:",text)
+
+t_list = splitter.splitter("en",text)
+
+print("Python got ",len(t_list)," lines back, splittet as:")
+
+
+for i in range(len(t_list)):
+    print(i+1,":",t_list[i])
+
+
+print(*t_list, sep = ":\n")
+
+print("------------------------")
+
+text='This is anoter text. We have sentences but no linebreaks. 3 sentences.'
+
+t_list = splitter.splitter("en",text)
+
+for i in range(len(t_list)):
+    print(i+1,":",t_list[i])
